26031eae33913ec9755c1a879d1ddbfbece95204
tyqiangz <40680129+tyqiangz@users.noreply.github.com>
2023-10-06 01:29:23 +0800

    tried using relative imports in tests/unit

--- tests/unit/mockbot.py
+++ tests/unit/mockbot.py
@@ -1,15 +1,7 @@
 # run this file to get a telegram bot running.
 
-from pathlib import Path
-import sys
+from src.bot import application
 from telegram import Update
 
-# directory reach
-directory = Path(__file__).absolute().parent.parent.parent / "src"
-
-# setting path
-sys.path.insert(0, str(directory))
-
-from bot import application
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -1,16 +1,7 @@
-from pathlib import Path
-import sys
-
-# directory reach
-directory = Path(__file__).absolute().parent.parent.parent / "src"
- 
-# setting path
-sys.path.insert(0, str(directory))
-
-from bot import _echo, _start
+from src.bot import application
 
 def test_echo():
-    assert _echo("hello") == "hello"
+    assert bot._echo("hello") == "hello"
 
 def test_start():
-    assert _start() == "Hello, I'm your personal assistant <3 How can I help you?"
+    assert bot._start() == "Hello, I'm your personal assistant <3 How can I help you?"
