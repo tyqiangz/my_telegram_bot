9cb9953e40cf84b4c5b8a0f6121d4abb8ba9a85d
tyqiangz <40680129+tyqiangz@users.noreply.github.com>
2023-10-15 01:02:55 +0800

    changed test for start command

--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -6,8 +6,8 @@ from ...my_telegram_bot.bot import _start
 
 def test_start():
     """Test if the `_start` function will return
-    'Hello, I'm your personal assistant <3 How can I help you?'
+    'Hello, I'm your personal assistant 😊 How can I help you?'
     as output.
     """
     assert _start(
-    ) == "Hello, I'm your personal assistant <3 How can I help you?"
+    ) == "Hello, I'm your personal assistant 😊 How can I help you?"
